c08a8451267b91c02f45b641b2f840e93909331e
NordTort <41715818+NorthTort@users.noreply.github.com>
2023-02-19 18:48:56 +0300

    The project has been created

---
+++ src/main/java/com/example/drivelinks/DrivelinksApplication.java
@@ -0,0 +1,13 @@
+package com.example.drivelinks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrivelinksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrivelinksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/drivelinks/DrivelinksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.drivelinks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrivelinksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
